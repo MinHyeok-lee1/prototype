24eb02267e57eefb264c2f0739124462d281cc0d
MinHyeok-lee1 <78718074+MinHyeok-lee1@users.noreply.github.com>
2021-12-23 21:38:14 +0900

    Add files via upload

---
+++ action.py
@@ -0,0 +1,125 @@
+import cv2
+import mediapipe as mp
+import pyfirmata
+import numpy as np
+
+def move_servo(motor, v):
+    motor.write(v)
+    board.pass_time(0.01)
+
+def calculate_angle2(a,b,c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+
+    angle = np.abs(angle*-1 + 180)
+    return angle
+
+def calculate_angle(a,b,c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(radians*180.0/np.pi)
+    
+    return angle
+
+mp_drawing = mp.solutions.drawing_utils
+mp_holistic = mp.solutions.holistic
+mp_hands = mp.solutions.hands
+
+board = pyfirmata.Arduino('COM7')
+servo1 = board.get_pin('d:2:s') # 팔꿈치
+servo2 = board.get_pin('d:3:s') # 어께임
+servo3 = board.get_pin('d:4:s') # 손가락임
+
+move_servo(servo1, 180)
+move_servo(servo2, 90)
+move_servo(servo3, 180)
+
+cap = cv2.VideoCapture(0)
+
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+
+        results = holistic.process(image)
+
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        
+        try:
+            landmarks = results.pose_landmarks.landmark
+            landmarks2 = results.left_hand_landmarks.landmark
+
+            left_shoulder = [landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_holistic.PoseLandmark.LEFT_SHOULDER.value].y]
+            right_shoulder = [landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER.value].y]
+            left_finger_tip = [landmarks2[8].x, landmarks2[8].y]
+            left_thumb_tip = [landmarks2[4].x, landmarks2[4].y]
+            elbow = [landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].x, landmarks[mp_holistic.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_holistic.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_holistic.PoseLandmark.LEFT_WRIST.value].y]
+
+            angle1 = calculate_angle(left_shoulder, elbow, wrist)
+            angle2 = calculate_angle(right_shoulder,left_shoulder,elbow)
+            angle3 = calculate_angle2(left_finger_tip,wrist,left_thumb_tip)
+
+            cv2.putText(image, str(angle1),
+                            tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA
+                                )
+            
+            cv2.putText(image, str(angle2),
+                            tuple(np.multiply(left_shoulder, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA
+                                )
+            
+            cv2.putText(image, str(angle3),
+                            tuple(np.multiply(left_finger_tip, [640, 480]).astype(int)),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2, cv2.LINE_AA
+                                )
+            if angle1 < 160:
+                move_servo(servo1, round(angle1))
+            move_servo(servo2, round(angle2))
+            move_servo(servo3, round(angle3))
+
+        except:
+            pass
+        # 1. Pose Detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                )
+    
+        # 2. Right hand
+        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                                )
+
+        # 3. Left Hand
+        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                                mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                                )
+        
+        cv2.imshow('Raw Webcam Feed', image)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            move_servo(servo1, 180)
+            move_servo(servo2, 90)
+            move_servo(servo3, 180)
+            break
+        
+
+cap.release()
+cv2.destroyAllWindows()
+board.exit()
